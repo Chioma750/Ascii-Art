105ec1f89b3b6ffb2f5b5a8e0feae91448a705aa
tterkimbi <terungwad34@gmail.com>
2026-05-24 09:33:24 +0100

    Buildart

---
+++ buildart.go
@@ -0,0 +1,23 @@
+package ascii
+
+import "strings"
+
+func BuildArt(str string, text []string) string {
+	validated := ValidateInput(str)
+	splistr := Splitting(validated)
+	var words strings.Builder
+	for _, char := range splistr {
+		if char == "" {
+			return words.String()
+		}
+		for i := range 8 {
+			for _, cha := range char {
+				stat := (int(cha)-32)*9 + 1
+				word := text[stat : stat+8]
+				words.WriteString(word[i])
+			}
+			words.WriteString("\n")
+		}
+	}
+	return words.String()
+}
